32df849385e63ad12b4c85f356be80f374805eb6
Jan van Waaij <jvanwaaij@gmail.com>
2021-06-26 14:01:06 +0200

    Update documentation. Remove unnecessary type annotations.

--- src/version0dot7and1dotX/samplepath.jl
+++ src/version0dot7and1dotX/samplepath.jl
@@ -10,20 +10,21 @@ Its subtype implements a continuous samplepath.
 abstract type AbstractSamplePath end
 
 """
-    isincreasing(x::AbstractVector{T})::Bool where T <: Number
-    isincreasing(x::AbstractRange{T})::Bool where T <: Number
+    isstrictlyincreasing(x::AbstractVector{T})::Bool where T <: Number
+    isstrictlyincreasing(x::AbstractRange{T})::Bool where T <: Number
 
-Tests whether a vector of numbers is strictly increasing. Is internal to
-NonparametricBayesForDiffusions.
+Internal function, not exported.
+
+Tests whether a vector of numbers is strictly increasing.
 """
-function isincreasing(x)
+function isstrictlyincreasing(x)
   for i in 1:length(x)-1
     x[i+1] <= x[i] && return false
   end
   return true
 end
 
-isincreasing(x::AbstractRange) = step(x) > 0
+isstrictlyincreasing(x::AbstractRange) = step(x) > 0
 
 """
     SamplePath(timeinterval::S, samplevalues::T) where
@@ -31,7 +32,7 @@ isincreasing(x::AbstractRange) = step(x) > 0
 
  Implements a continuous samplepath where the timeinterval is not necessarily
  equally spaced. Sample value samplevalues[k] is the value of the process at
- time timeinterval[k]. timeinterval is an increasing vector. timeinterval and
+ time timeinterval[k]. timeinterval is a strictly increasing vector. timeinterval and
  samplevalues are of equal length.
 
  # Performance hint: 
@@ -69,7 +70,7 @@ struct SamplePath{S, T} <: AbstractSamplePath where {S<:AbstractVector{Float64},
     length(timeinterval) == length(samplevalues) ||
       throw(DimensionMismatch("Length of timeinterval should be equal to the length of the
         samplevalues vector"))
-    isincreasing(timeinterval) ||
+    isstrictlyincreasing(timeinterval) ||
       throw(ArgumentError("Timeinterval should be increasing"))
     new{S,T}(timeinterval, samplevalues)
   end
@@ -79,7 +80,7 @@ end
 SamplePath(timeinterval, f::Function) = SamplePath(timeinterval, f.(timeinterval))
 
 """
-    Base.step(X::SamplePath{S}) where S<:AbstractRange{T} where T<:Number
+    Base.step(X::SamplePath{<:AbstractRange})
 
 Returns the step size of SamplePath.timeinterval.
 
@@ -91,7 +92,7 @@ X = SamplePath(t, sinpi)
 step(X) == step(t)
 ```
 """
-Base.step(X::SamplePath{S}) where S<:AbstractRange{T} where T<:Number = Base.step(X.timeinterval)
+Base.step(X::SamplePath{<:AbstractRange}) = (println("hi"); Base.step(X.timeinterval))
 
 """
     Base.length(X::SamplePath)
